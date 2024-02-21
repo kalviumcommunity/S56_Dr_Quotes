6ba2783ef53861a934420b577308d5b2fce03bae
Dhruv Unmesh Patil <universo_esplorar@Dhruvs-MacBook-Air.local>
2024-02-21 13:46:23 +0530

    Added Helloe World

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-// server.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.get('/ping', (req, res) => {
   res.send('Ping');
